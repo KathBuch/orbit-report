18f290949ca9297402ccc4a36e5e2239776c68a2
Katie <kathrynb1213@gmail.com>
2020-08-09 22:43:06 -0500

    Step 4 Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Satellite } from './satellite';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'orbit-report';
-sourceList: Satellite[];
+  sourceList: Satellite[];
   constructor() {
     this.sourceList = [
        new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
